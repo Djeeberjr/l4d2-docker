b3c78a24379670d0b0edda7cc0217652a6d93615
Niklas <niklas@kapelle.org>
2021-04-12 17:11:45 +0200

    more start parameters

--- start.sh
+++ start.sh
@@ -10,8 +10,8 @@ echo "### Creating config ###"
 
 cd $HOME/server
 
-env | awk -F "=" '/^CVAR_/ {sub("CVAR_","",$1); print tolower($1),($2 ~ /^[0-9]+$/)?$2:"\""$2"\""}' > docker_server.cfg
+env | awk -F "=" '/^CVAR_/ {sub("CVAR_","",$1); print tolower($1),($2 ~ /^[0-9]+$/)?$2:"\""$2"\""}' > server.cfg
 
 echo "### Starting l4d2 ###"
 
-$HOME/server/srcds_run -console -game left4dead2 +maxplayers 16 -maxclients 16 +map c1m1_hotel +exec docker_server.cfg
+$HOME/server/srcds_run -console -game left4dead2 +maxplayers 16 -maxclients 16 -ip 0.0.0.0 +map c1m1_hotel
