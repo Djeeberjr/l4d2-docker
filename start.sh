a9c28b89ea64f0ea88364b9e349fdbf57e9a014e
Niklas <niklas@kapelle.org>
2021-04-18 15:25:45 +0200

    motd and host file

--- start.sh
+++ start.sh
@@ -13,6 +13,22 @@ cp $HOME/server.cfg $HOME/server/left4dead2/cfg/server.cfg
 
 env | awk -F "=" '/^CVAR_/ {sub("CVAR_","",$1); print tolower($1),($2 ~ /^[0-9]+$/)?$2:"\""$2"\""}' > docker_server.cfg
 
+echo "### Using follwing config: "
+cat $HOME/server/left4dead2/cfg/server.cfg
+echo "###"
+
+echo "### Linking host and motd file ### "
+
+if [[ -f "/motd.txt" ]]; then
+    rm $HOME/server/left4dead2/motd.txt
+    ln -sf /motd.txt $HOME/server/left4dead2/motd.txt
+fi
+
+if [[ -f "/host.txt" ]]; then
+    rm $HOME/server/left4dead2/host.txt
+    ln -sf /host.txt $HOME/server/left4dead2/host.txt
+fi
+
 echo "### Starting l4d2 ###"
 
 $HOME/server/srcds_run -console -game left4dead2 +maxplayers 16 -maxclients 16 -ip 0.0.0.0 +map c1m1_hotel +exec docker_server.cfg
